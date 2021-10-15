71c3880159ffd39c81921a4bf66aa7a12307761a
Przemyslaw Koza <pkoza@pulawy.com>
2021-10-15 14:05:45 +0200

    initial version

---
+++ src/xml_transformer/core.clj
@@ -0,0 +1,45 @@
+(ns xml-transformer.core
+  (:require [clojure.xml :as xml]
+            [clojure.data.zip.xml :as zip-xml]
+            [clojure.zip :as zip]))
+
+(set! *warn-on-reflection* true)
+
+(defn parse-str
+  "Parses string to tree of the xml/element struct-map zipped with xml-zip"
+  [s]
+  (-> (xml/parse (new org.xml.sax.InputSource
+                      (new java.io.StringReader s)))
+      zip/xml-zip))
+
+
+(defn remove-nils [m]
+  (into {} (remove (comp nil? second) m)))
+
+(defn transform-xml
+  "Transforms zipped-xml into map using field-map"
+  [zipped-xml field-map]
+  (let [as-map (->>
+                (for [[field selector] field-map]
+                  (let [many?          (-> selector meta :many)
+                        coerce-fn      (-> selector meta :coerce)
+                        coerce-node-fn (-> selector meta :coerce-node)
+                        f              (if many? zip-xml/xml-> zip-xml/xml1->)
+                        value          (apply f zipped-xml (conj selector zip-xml/text))
+                        node           (apply f zipped-xml selector)
+
+                        value
+                        (if (and value (or coerce-fn coerce-node-fn))
+                          (if coerce-fn
+                            (if (coll? value)
+                              (mapv coerce-fn value)
+                              (coerce-fn value))
+                            ;; coerce-node? is true
+                            (if many?
+                              (mapv coerce-node-fn node)
+                              (coerce-node-fn node)))
+                          value)]
+                    [field value]))
+                (into {}))
+        as-map (remove-nils as-map)]
+    as-map))
---
+++ test/xml_transformer/core_test.clj
@@ -0,0 +1,62 @@
+(ns xml-transformer.core-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [xml-transformer.core :refer [transform-xml parse-str]]))
+
+(def flat-xml
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<head>
+<attr1>value1</attr1>
+<attr2>value2</attr2>
+<attr3>42</attr3>
+</head>")
+
+(def nested-xml
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<head>
+<attr1>value1</attr1>
+<attr2>value2</attr2>
+<attr3>42</attr3>
+<lines>
+<line><lineattr1>line1 value1</lineattr1></line>
+<line><lineattr1>line2 value1</lineattr1><lineattr2>123</lineattr2></line>
+</lines>
+</head>")
+
+(def fields1
+  {:header/attr1 [:head :attr1]
+   :header/attr2 [:head :attr2]})
+
+(def fields2
+  {:header/attr3 ^{:coerce #(Integer/parseInt %)}
+   [:head :attr3]})
+
+(declare line-fields)
+
+(def fields3
+  {:header/attr1 [:head :attr1]
+   :header/attr2 [:head :attr2]
+   :header/attr3 ^{:coerce #(Integer/parseInt %)}
+   [:head :attr3]
+   :lines ^{:many true :coerce-node #(transform-xml % line-fields)}
+   [:lines :line]})
+
+(def line-fields
+  {:line/attr1
+   [:lineattr1]
+   :line/number ^{:coerce #(Integer/parseInt %)}
+   [:lineattr2]})
+
+(deftest flat-transform
+  (testing "Transformation of flat xml"
+    (is (= {:header/attr1 "value1", :header/attr2 "value2"}
+           (transform-xml (parse-str flat-xml) fields1))))
+  (testing "Transformation of flat xml with coercion"
+    (is (= {:header/attr3 42}
+           (transform-xml (parse-str flat-xml) fields2))))
+  (testing "Transformation of nested xml with node coercion"
+    (is (= {:header/attr1 "value1",
+            :header/attr2 "value2",
+            :header/attr3 42,
+            :lines        [#:line{:attr1 "line1 value1"}
+                           #:line{:attr1 "line2 value1" :number 123}]}
+           (transform-xml (parse-str nested-xml) fields3)))))
